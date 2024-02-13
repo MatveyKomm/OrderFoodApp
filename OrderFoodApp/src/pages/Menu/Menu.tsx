4f487c63bc217a7da0b060fbeb945ee993be89db
Matvey Kommercheskiy <kommercheskiy.m@wb.ru>
2024-02-13 16:49:33 +0300

    added product card and route

--- OrderFoodApp/src/pages/Menu/Menu.tsx
+++ OrderFoodApp/src/pages/Menu/Menu.tsx
@@ -1,12 +1,23 @@
 import Headling from "../../components/Headling/Headling.tsx";
 import Search from "../../components/Search/Search.tsx";
 import styles from './Menu.module.css';
+import ProductCard from "../../components/ProductCard/ProductCard.tsx";
 
 export function Menu() {
     return <>
         <div className={styles['head']}>
             <Headling>Menu</Headling>
             <Search placeholder='Введите блюдо или состав' />
+        </div>
+        <div>
+            <ProductCard
+                id={1}
+                title={'Pleasure'}
+                description={'Tomato'}
+                image={'/example-icon.png'}
+                price={300}
+                rating={5}
+            />
         </div>
     </>;
 }
