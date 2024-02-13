5103f0c2a8d3a5e76ca2286ff7598afc3d6abc49
Matvey Kommercheskiy <kommercheskiy.m@wb.ru>
2024-02-13 10:04:28 +0300

    added search

---
+++ OrderFoodApp/src/components/Search/Search.tsx
@@ -0,0 +1,18 @@
+import styles from './Search.module.css';
+import cn from 'classnames';
+import {forwardRef} from "react";
+import {SearchProps} from "./Search.props.ts";
+
+const Search = forwardRef<HTMLInputElement, SearchProps>(function Input({className, isValid = true, ...props}, ref) {
+
+    return (
+        <div className={styles['input-wrapper']}>
+            <input ref={ref} className={cn(styles['input'], className, {
+                [styles['invalid']]: isValid
+            })} {...props} />
+            <img className={styles['icon']} src='/search-icon.svg' alt='Иконка поиска' />
+        </div>
+    );
+})
+
+export default Search;
--- OrderFoodApp/src/pages/Menu/Menu.tsx
+++ OrderFoodApp/src/pages/Menu/Menu.tsx
@@ -1,3 +1,12 @@
+import Headling from "../../components/Headling/Headling.tsx";
+import Search from "../../components/Search/Search.tsx";
+import styles from './Menu.module.css';
+
 export function Menu() {
-    return <>Menu</>;
+    return <>
+        <div className={styles['head']}>
+            <Headling>Menu</Headling>
+            <Search placeholder='Введите блюдо или состав' />
+        </div>
+    </>;
 }
